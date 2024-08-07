e409ce6b2eb4c32d5f9d926a80acd9c185c6da6a
Koulie <kouliekyriakos@hotmail.com>
2024-08-07 09:00:34 -0400

    Can now add images to the carocel

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import './style.css';
+import picture1 from './images/images-for-carocel/picture1.jpeg';
+import picture2 from './images/images-for-carocel/picture2.jpeg';
+import picture3 from './images/images-for-carocel/picture3.jpeg';
+
+
+// let image = document.createElement('img');
+// image.src = picture1;
+// imageParent.appendChild(image);
+
+// let imageFolder = './images/images-for-carocel/'
+// let imageList = [
+//     'picture1.jpeg',
+//     'picture2.jpeg',
+//     'picture3.jpeg'
+// ]
+const imageParent = document.querySelector('.image-container');
+
+let fullImagePaths = [
+    picture1,
+    picture2,
+    picture3
+]
+function addImagesToContainer(parent, fullImagePaths){
+    fullImagePaths.forEach((image) => {
+        let imageHolder = document.createElement('img');
+        imageHolder.src = image;
+        parent.appendChild(imageHolder);
+    })
+}
+if (imageParent != null){
+    addImagesToContainer(imageParent, fullImagePaths);
+} else {
+    console.log('ImageParent is null');
+}
+
+
+//Button
+let goNextButton = document.querySelector('#go-next');
+goNextButton.addEventListener('click', () => {
+    console.log('button clicked');
+})
